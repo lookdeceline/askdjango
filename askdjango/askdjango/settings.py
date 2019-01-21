f65d537186b293f37fa7e3ca282871ab3df1a711
lookdeceline <peaceluverceline@gmail.com>
2019-01-21 13:25:12 +0900

    VOD1 lecture2_2

--- askdjango/askdjango/settings.py
+++ askdjango/askdjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')wo5x#k#f(-%5j^(d85(qs*xgasfjtkppc)b540#dcb4oo8qo+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*',]
 
 
 # Application definition
--- askdjango/askdjango/urls.py
+++ askdjango/askdjango/urls.py
@@ -20,5 +20,6 @@ from django.conf.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
+    path('', include('blog.urls')),
 
 ]
