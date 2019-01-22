81cc74e7ef73ba45e413433111eeaee1a4e0495d
lookdeceline <peaceluverceline@gmail.com>
2019-01-22 20:25:51 +0900

    customizing admin register

--- askdjango/blog/admin.py
+++ askdjango/blog/admin.py
@@ -3,9 +3,9 @@ from .models import Post
 # Register your models here.
 
 # admin에 등록 방법들
-
-# 등록법 1
-admin.site.register(Post)
+#
+# # 등록법 1
+# admin.site.register(Post)
 
 #
 # # 등록법 2
@@ -19,3 +19,15 @@ admin.site.register(Post)
 # @admin.register(Post)
 # class PostAdmin(admin.ModelAdmin):
 #     list_display = ['id', 'title', 'created_at', 'updated_at']
+
+
+###################################
+####customizing admin register#####
+###################################
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'content_size','created_at', 'updated_at']
+
+    def content_size(self, post):
+        return '{} words'.format(len(post.content))
+    content_size.short_description = "글자수"  # renaming
