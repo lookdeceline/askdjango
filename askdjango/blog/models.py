8bced5d4e1893b7c52f354f4db82324025fd2b9a
lookdeceline <peaceluverceline@gmail.com>
2019-01-22 17:32:44 +0900

    first models Post

--- askdjango/blog/models.py
+++ askdjango/blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100, help_text="title of article") # verbose_name = "제목"
+    content = models.TextField() # verbose_name = "내용"
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now = True)
