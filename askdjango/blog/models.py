aa405a27c8234d8c54685531a567cbc18dbec2be
lookdeceline <peaceluverceline@gmail.com>
2019-01-22 19:52:28 +0900

    migrating an app with models with required field

--- askdjango/blog/models.py
+++ askdjango/blog/models.py
@@ -9,6 +9,7 @@ def lnglat_validator(value):
 
 
 class Post(models.Model):
+    author = models.CharField(max_length=20) #blank, null options False by default
     title = models.CharField(max_length=100, help_text="title of article") # verbose_name = "제목"
     content = models.TextField() # verbose_name = "내용"
     created_at = models.DateTimeField(auto_now_add=True)
