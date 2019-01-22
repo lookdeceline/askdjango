fbe58fed181f71cbe3e11164ea57d394204c9397
lookdeceline <peaceluverceline@gmail.com>
2019-01-22 20:17:24 +0900

    VOD1 lecture8

--- askdjango/blog/admin.py
+++ askdjango/blog/admin.py
@@ -1,4 +1,21 @@
 from django.contrib import admin
 from .models import Post
 # Register your models here.
+
+# admin에 등록 방법들
+
+# 등록법 1
 admin.site.register(Post)
+
+#
+# # 등록법 2
+# class PostAdmin(admin.ModelAdmin):
+#     list_display=['id', 'title', 'created_at', 'updated_at']
+#
+# admin.site.register(Post, PostAdmin)
+#
+#
+# # 등록법 3
+# @admin.register(Post)
+# class PostAdmin(admin.ModelAdmin):
+#     list_display = ['id', 'title', 'created_at', 'updated_at']
