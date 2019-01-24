bf5a0b30eff3c592e350f875ddcb8535b17ece90
lookdeceline <peaceluverceline@gmail.com>
2019-01-24 14:18:40 +0900

    VOD1 lecture9 blog.views.post_list views.py implemented QuerySet SEARCH

--- askdjango/blog/views.py
+++ askdjango/blog/views.py
@@ -4,6 +4,11 @@ from .models import Post
 # Create your views here.
 def post_list(request):
     qs = Post.objects.all()
+    q = request.GET.get('q', '')
+    if q:
+        qs = qs.filter(title__icontains = q)
+
     return render(request, 'blog/post_list.html', {
         'post_list': qs,
+        'q': q,
     })
